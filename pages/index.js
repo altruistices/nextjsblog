23f39fd9a75a9fd1f9cc071482353e5a53b42ea4
Shanu Jha <shanu.j@foxberry.in>
2020-10-06 00:21:43 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import { getSortedPostsData } from '../lib/posts'
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello I am Shanu. I am a <i>Solutions Developer</i> @ Foxberry Technologies. I usually do magic with software</p>
+        <p>
+          I have several projects on GitHub. I &lt;3 <a href="nextjs.org">Next.js</a>        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
---
+++ posts/why-this-blog.md
@@ -0,0 +1,6 @@
+---
+title: 'Why this blog'
+date: '2020-03-13'
+---
+
+Swing it or not! This is working very very well.
